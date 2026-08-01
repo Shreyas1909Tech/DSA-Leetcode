cf65eab525e1bac1367ad8806a03bb65f77dcaec
Deobhankar Shreyas Sunil <114716025+Shreyas1909Tech@users.noreply.github.com>
2026-08-01 17:15:15 +0530

    Time: 149 ms (63.05%), Space: 15.9 MB (53.31%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -3,36 +3,33 @@ public:
     int countPalindromicSubsequence(string s) {
     int n=s.length();
 
-    unordered_set<char>letters;
+    vector<pair<int,int>>indices(26,{-1,-1});
+    int result=0;
 
     for(int i=0;i<n;i++)
     {
-        letters.insert(s[i]);
-    }    
-    int result=0;
-    for(char letter:letters)
-    {
-        int left_idx=-1;
-        int right_idx=-1;
-    
-    for(int i=0;i<n;i++)
-    {
-        if(s[i]==letter)
+        char ch=s[i];
+        int idx=ch-'a';
+        if(indices[idx].first==-1)
         {
-            if(left_idx==-1)
-            {
-              left_idx=i;  
-            }
-         right_idx=i;
+            indices[idx].first=i;
         }
-       
+        indices[idx].second=i;
+    }
+    for(int i=0;i<26;i++)
+    {
+    int left_idx=indices[i].first;
+    int right_idx=indices[i].second;
+  
+    if(left_idx==-1)
+    {
+        continue;
     }
     unordered_set<char>st;
     for(int middle=left_idx+1;middle<=right_idx-1;middle++)
     {
         st.insert(s[middle]);
     }
-
     result+=st.size();
     }
     return result;
