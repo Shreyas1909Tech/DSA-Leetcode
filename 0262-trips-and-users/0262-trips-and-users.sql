b1279a2207965d55022535f1e54273015867394c
Deobhankar Shreyas Sunil <114716025+Shreyas1909Tech@users.noreply.github.com>
2026-09-04 19:43:10 +0530

    Time: 694 ms (27.28%), Space: 0B (100%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,11 @@
+SELECT 
+    t.request_at AS Day,
+    ROUND(
+        SUM(CASE WHEN t.status IN ('cancelled_by_client', 'cancelled_by_driver') THEN 1 ELSE 0 END) / COUNT(*), 
+        2
+    ) AS "Cancellation Rate"
+FROM Trips AS t
+JOIN Users AS u1 ON t.client_id = u1.users_id AND u1.banned = 'No'
+JOIN Users AS u2 ON t.driver_id = u2.users_id AND u2.banned = 'No'
+WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at;
