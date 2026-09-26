7dc2d39f642e1e09df67be12454bc49a6f97e50c
Deobhankar Shreyas Sunil <shreyasdeobhankar1909@gmail.com>
2026-09-26 20:30:30 +0530

    Time: 1171 ms (95.66%), Space: 0B (100%) - LeetHub

---
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    CASE
+        WHEN S.product_id=p.product_id THEN p.product_name
+        ELSE 'Unknown'
+    END AS product_name,
+    s.year,
+    s.price
+FROM 
+    Sales s
+LEFT JOIN
+    Product p ON s.product_id=p.product_id;
